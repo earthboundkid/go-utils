a386281ab3f827cd344b8fbffc31d3ed1129f22b
Carl Johnson <cmjohnson@pbs.org>
2015-12-17 22:46:55 -0500

    Add a simple semaphore type

---
+++ semaphore/sem.go
@@ -0,0 +1,41 @@
+package semaphore
+
+import "sync"
+
+type Semaphore struct {
+	sem  chan struct{}
+	done chan struct{}
+	once sync.Once
+}
+
+func New(n int) *Semaphore {
+	return &Semaphore{
+		sem:  make(chan struct{}, n),
+		done: make(chan struct{}),
+	}
+}
+
+func (s *Semaphore) Acquire() bool {
+	select {
+	case s.sem <- struct{}{}:
+		return true
+	case <-s.done:
+		return false
+	}
+}
+
+func (s *Semaphore) Release() {
+	go func() {
+		select {
+		case <-s.sem:
+		case <-s.done:
+		}
+	}()
+}
+
+func (s *Semaphore) Stop() {
+	s.once.Do(func() {
+		close(s.done)
+		s.sem = nil
+	})
+}
