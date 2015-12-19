0ffcc05d7e9c8ac5d51cafb507d9006046a59da8
Carl Johnson <cmjohnson@pbs.org>
2015-12-19 11:05:27 -0500

    Semaphore: Add documentation

--- semaphore/sem.go
+++ semaphore/sem.go
@@ -1,13 +1,21 @@
+// Package semaphore is a package containing a simple reference
+// implementation of a semaphore type. If you need to a semaphore in
+// your own package, probably the right way to use this package is to
+// copy it and make your own specialized implementation.
 package semaphore
 
 import "sync"
 
+// A Semaphore helps add restrictions on the number of active goroutines
+// by ensuring that no more than n tokens may be acquired at one time.
+// It also allows for the broadcasting of Stop messages.
 type Semaphore struct {
 	sem  chan struct{}
 	done chan struct{}
 	once sync.Once
 }
 
+// New creates a new Semaphore with n max number of tokens allowed.
 func New(n int) *Semaphore {
 	return &Semaphore{
 		sem:  make(chan struct{}, n),
@@ -15,6 +23,8 @@ func New(n int) *Semaphore {
 	}
 }
 
+// Acquire returns true after it acquires a token from the underlying
+// Semaphore or false if the Semaphore has been closed with Stop().
 func (s *Semaphore) Acquire() bool {
 	select {
 	case s.sem <- struct{}{}:
@@ -24,6 +34,8 @@ func (s *Semaphore) Acquire() bool {
 	}
 }
 
+// Release is a non-blocking operation to return a Semaphore token. It
+// is safe to call after the Semaphore has been closed with Stop().
 func (s *Semaphore) Release() {
 	go func() {
 		select {
@@ -33,6 +45,8 @@ func (s *Semaphore) Release() {
 	}()
 }
 
+// Stop closes its underlying Semaphore. It is safe to call multiple
+// times.
 func (s *Semaphore) Stop() {
 	s.once.Do(func() {
 		s.sem = nil
