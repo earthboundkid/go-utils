4a89ea4500fd36086167e4effd0997b5388ed120
Carl Johnson <cmjohnson@pbs.org>
2015-12-19 11:28:54 -0500

    Semaphore: Add a demonstration

---
+++ semaphore/internal/demo-main.go
@@ -0,0 +1,41 @@
+// Demonstration of Semaphore package. No more than N routines should be
+// running at any one time.
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+
+	"github.com/carlmjohnson/go-utils/semaphore"
+)
+
+// N is the number of active semaphores allowed.
+const N = 3
+
+var (
+	s = semaphore.New(N)
+	w = new(int32)
+)
+
+func worker(n int) {
+	for s.Acquire() {
+		fmt.Printf("%s\t%d\t%*s\n",
+			time.Now().Format("15:04:05"), atomic.AddInt32(w, 1), n, "^")
+		time.Sleep(1 * time.Second)
+		fmt.Printf("%s\t%d\t%*s\n",
+			time.Now().Format("15:04:05"), atomic.AddInt32(w, -1), n, "_")
+		s.Release()
+	}
+	fmt.Printf("%s\t%d\t%*s\n", time.Now().Format("15:04:05"), *w, n, "*")
+}
+
+func main() {
+	fmt.Println("Time\t# of workers\n")
+	for i := 0; i < 10; i++ {
+		go worker(5 * i)
+	}
+	time.Sleep(10 * time.Second)
+	s.Stop()
+	time.Sleep(2 * time.Second)
+}
