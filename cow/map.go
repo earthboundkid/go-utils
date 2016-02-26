c8b032b7f13b520fb727dc674c5d3358de6e7d3c
Carl Johnson <cmjohnson@pbs.org>
2016-02-26 13:57:42 -0500

    Add copy-on-write map
    Based on the atomic.Value example in Go's documentation.

---
+++ cow/map.go
@@ -0,0 +1,73 @@
+// Package cow provides a reference implementation of copy-on-write maps
+// for read-heavy, write-light data.
+package cow
+
+import (
+	"sync"
+	"sync/atomic"
+)
+
+// Map is a synchronous copy on write map. Reads are cheap. Writes are expensive.
+type Map struct {
+	v atomic.Value
+	m sync.Mutex // used only by writers
+}
+
+func copyM(dst map[string]string, src map[string]string) {
+	for k, v := range src {
+		dst[k] = v // copy all data from the current object to the new one
+	}
+}
+
+func dup(src map[string]string) (dst map[string]string) {
+	dst = make(map[string]string)
+	copyM(dst, src)
+	return dst
+}
+
+// New initializes a new map based on m.
+func New(m map[string]string) *Map {
+	cowm := Map{}
+	cowm.v.Store(dup(m))
+	return &cowm
+}
+
+// Get retreives the value associated with the key from the Map.
+func (cowm *Map) Get(key string) (val string) {
+	// No lock needed!
+	m := cowm.v.Load().(map[string]string)
+	return m[key]
+}
+
+// Insert inserts a key-value pair.
+func (cowm *Map) Insert(key, val string) {
+	cowm.m.Lock()
+	defer cowm.m.Unlock()
+
+	src := cowm.v.Load().(map[string]string)
+	dst := dup(src)
+	dst[key] = val
+	cowm.v.Store(dst)
+}
+
+// Update efficiently inserts all the values in m into the Map.
+func (cowm *Map) Update(m map[string]string) {
+	cowm.m.Lock()
+	defer cowm.m.Unlock()
+
+	src := cowm.v.Load().(map[string]string)
+	dst := dup(src)
+	copyM(dst, m)
+	cowm.v.Store(dst)
+}
+
+// Remove removes key from the Map.
+func (cowm *Map) Remove(key string) {
+	cowm.m.Lock()
+	defer cowm.m.Unlock()
+
+	src := cowm.v.Load().(map[string]string)
+	dst := dup(src)
+	delete(dst, key)
+	cowm.v.Store(dst)
+}
---
+++ cow/map_test.go
@@ -0,0 +1,81 @@
+package cow_test
+
+import (
+	"testing"
+
+	"github.com/carlmjohnson/go-utils/cow"
+)
+
+var m = map[string]string{"a": "a"}
+
+func BenchmarkNew(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = cow.New(nil)
+	}
+}
+
+func BenchmarkPlainRead(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = m["a"]
+	}
+}
+
+func BenchmarkRead(b *testing.B) {
+	c := cow.New(m)
+	for i := 0; i < b.N; i++ {
+		c.Get("a")
+	}
+}
+
+func BenchmarkInsert(b *testing.B) {
+	c := cow.New(m)
+	for i := 0; i < b.N; i++ {
+		c.Insert("a", "a")
+	}
+}
+
+func BenchmarkUpdate(b *testing.B) {
+	c := cow.New(m)
+	for i := 0; i < b.N; i++ {
+		c.Update(m)
+	}
+}
+
+func BenchmarkRemove(b *testing.B) {
+	c := cow.New(m)
+	for i := 0; i < b.N; i++ {
+		c.Remove("x")
+	}
+}
+
+func BenchmarkGoRead(b *testing.B) {
+	c := cow.New(m)
+	for i := 0; i < b.N; i++ {
+		go c.Get("a")
+		c.Get("a")
+	}
+}
+
+func BenchmarkGoInsert(b *testing.B) {
+	c := cow.New(m)
+	for i := 0; i < b.N; i++ {
+		go c.Insert("a", "a")
+		c.Insert("a", "a")
+	}
+}
+
+func BenchmarkGoUpdate(b *testing.B) {
+	c := cow.New(m)
+	for i := 0; i < b.N; i++ {
+		go c.Update(m)
+		c.Update(m)
+	}
+}
+
+func BenchmarkGoRemove(b *testing.B) {
+	c := cow.New(m)
+	for i := 0; i < b.N; i++ {
+		go c.Remove("x")
+		c.Remove("x")
+	}
+}
